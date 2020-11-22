c18faa589c5de6e74180ee7b0c77737827efbb34
leonardoledo <ledo.leonardo@hotmail.com>
2020-11-22 14:46:09 -0300

    Another fix

--- scripts/pageColor.js
+++ scripts/pageColor.js
@@ -15,31 +15,7 @@ function initialPageColor() {
     } else {
         colorIndex = 0
     }
-    if (currentColor == 'brown' || currentColor == 'null') {
-    body.style.backgroundColor = '#7e624f'
-    containerPainel.style.backgroundColor = '#5a3c28'
-    cont.style.backgroundColor = '#462814'
-    document.getElementById('color').style.backgroundColor = '#50321e'
-        for(let i = 0; i < boxesBorderColor.length; i++){
-            boxesBorderColor[i].style.borderColor = '#8c6e5a'
-        }
-        for(let i = 0; i < inputs.length; i++) {
-            if (inputs[i].id == 'row' || inputs[i].id == 'nowrap' || inputs[i].id == 'stretch'){
-            inputs[i].style.backgroundColor = '#b4b43b8a'
-            } else {
-            inputs[i].style.backgroundColor = '#50321e'
-            }
-        }
-        for (let i = 0; i < inputsJC.length; i++) {
-            if (inputsJC[i].id == 'flex-start') {
-                inputsJC[i].style.backgroundColor = '#b4b43b8a'
-            }
-        }
-        for(let i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].style.backgroundColor = '#50321e'
-        }
-
-    } else {
+    if(currentColor == 'grey') {
     body.style.backgroundColor = '#AAA'
     containerPainel.style.backgroundColor = '#666'
     cont.style.backgroundColor = '#444'
@@ -61,6 +37,29 @@ function initialPageColor() {
         for(let i = 0; i < checkboxes.length; i++) {
             checkboxes[i].style.backgroundColor = '#555'
             }
+        }
+    } else {
+    body.style.backgroundColor = '#7e624f'
+    containerPainel.style.backgroundColor = '#5a3c28'
+    cont.style.backgroundColor = '#462814'
+    document.getElementById('color').style.backgroundColor = '#50321e'
+        for(let i = 0; i < boxesBorderColor.length; i++){
+            boxesBorderColor[i].style.borderColor = '#8c6e5a'
+        }
+        for(let i = 0; i < inputs.length; i++) {
+            if (inputs[i].id == 'row' || inputs[i].id == 'nowrap' || inputs[i].id == 'stretch'){
+            inputs[i].style.backgroundColor = '#b4b43b8a'
+            } else {
+            inputs[i].style.backgroundColor = '#50321e'
+            }
+        }
+        for (let i = 0; i < inputsJC.length; i++) {
+            if (inputsJC[i].id == 'flex-start') {
+                inputsJC[i].style.backgroundColor = '#b4b43b8a'
+            }
+        }
+        for(let i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].style.backgroundColor = '#50321e'
         }
     }
 }
